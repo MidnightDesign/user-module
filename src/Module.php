211395292bc7085a1d3fa8146d7986c0ec331e54
Rudolph Gottesheim <r.gottesheim@loot.at>
2014-05-23 12:56:35 +0200

    Add doctrine/doctrine-orm-module dependency

--- src/Module.php
+++ src/Module.php
@@ -2,10 +2,11 @@
 
 namespace Midnight\UserModule;
 
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
-class Module
+class Module implements DependencyIndicatorInterface
 {
     public function onBootstrap(MvcEvent $e)
     {
@@ -23,5 +24,15 @@ class Module
     public function getConfig()
     {
         return include dirname(__DIR__) . '/config/module.config.php';
+    }
+
+    /**
+     * Expected to return an array of modules on which the current one depends on
+     *
+     * @return array
+     */
+    public function getModuleDependencies()
+    {
+        return array('DoctrineORMModule');
     }
 }
